4eab9ad93bec9715e5ab4e58a355146a63806d91
Rob Donoghue <rdonoghue@gmail.com>
2016-04-13 12:52:44 -0400

    Exercise 36

--- cat.py
+++ cat.py
@@ -1,3 +1,14 @@
+def prepend(listname, prepend_value):
+    '''Reverses the list, appends the new value, then reverses it again.
+    Clunky and ungraceful, but it works until I find a really way to
+    prepend'''
+    listname.reverse()
+    listname.append(prepend_value)
+    listname.reverse()
+
+
+
+
 from sys import argv
 filename = argv[1]
 # filename = "textfile.txt"
---
+++ fizzbuz1.py
@@ -0,0 +1,8 @@
+mainlist = range(1,100)
+
+for count in mainlist:
+    realnum = count+1
+    if realnum % 3 == 0:
+        print "Fizz"
+    else:
+        print realnum
