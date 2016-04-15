ca9aa5bee524255f7b01a99a2f090b5f6bfd24cc
Rob Donoghue <rdonoghue@gmail.com>
2016-04-14 23:05:00 -0400

    The cards as calsses now words, can shuffle, whole nine yards, bu my shuffling algorihm is crap.  Need o think about that sometime

--- cards.py
+++ cards.py
@@ -1,3 +1,5 @@
+import random
+
 class Card(object):
 	"""Playing cards as a class"""
 	def __init__(self,face,suit,value):
@@ -20,21 +22,56 @@ class Card(object):
 		return "%s%s" % (self.value, ssuit)
 
 def filldeck():
-    '''creates a deck of cards (non manipulable)'''
-    mydeck=[];
-    suits=["clubs", "diamonds", "hearts", "spades"]
-    faces=["Jack","Queen","King","Ace"]
-    for suit in suits:
-        for num in range(2,11):
-            card="%s of %s" % (num, suit)
-            mydeck.append(card)
-        for face in faces:
-            card="%s of %s" % (face, suit)
-            mydeck.append(card)
-    return mydeck
+	mydeck=[]
+	suits=["clubs", "diamonds", "hearts", "spades"]
+	faces=["Jack","Queen","King","Ace"]
+	numwords = [
+	"Zero", "One", "Two", "Three",
+	"Four","Five", "Six", "Seven",
+	"Eight","Nine","Ten"
+	]
+	for suit in suits:
+		for num in range(2,11):
+			card = Card(num, suit,numwords[num])
+			mydeck.append(card)
+		for face in faces:
+			if face == "Jack":
+				sface="J"
+			elif face == "Queen":
+				sface="Q"
+			elif face == "King":
+				sface="K"
+			elif face == "Ace":
+				sface="A"
+			else:
+				sface="?"
+			card = Card(face, suit,sface)
+			mydeck.append(card)
+	return mydeck
 
-numwords=["Zero", "One","Two","Three","Four","Five","Six","Seven",
-		"Eight","Nine","Ten"]
+def shuffledeck(sdeck):
+    '''Randomly Reorders the Deck (or any list)'''
+    cards=len(sdeck)
+    print "Shuffling",cards,"cards"
+    for events in range(0,cards * 100):
+        pos=0
+        for marker in sdeck:
+            flip=random.randint(1,2)
+            if flip == 1:
+                npos=pos+1
+                if pos==cards - 1:
+                    npos=-1
+                sdeck[pos],sdeck[npos] = sdeck[npos],sdeck[pos]
+            pos += 1
+    return sdeck
+
+def dealcards(handsize):
+    thisdeck=filldeck()
+    thisdeck=shuffledeck(thisdeck)
+    thishand=[]
+    for draws in range(0,handsize):
+        thishand.append(thisdeck.pop(0))
+    return thishand
 
 card1=Card("Ace", "Hearts", "A")
 card2=Card("Queen", "Clubs", "Q")
@@ -44,4 +81,15 @@ deck=[card1,card2]
 print card1.showcard(),
 print "(%s)" % card1.shortcard()
 
+print "(%s)" % deck[1].shortcard()
+
 print "---"
+
+classdeck=filldeck()
+
+print classdeck[0].showcard()
+print len(classdeck)
+classdeck=shuffledeck(classdeck)
+myhand=dealcards(5)
+for card in myhand:
+	print card.showcard()
